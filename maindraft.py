0cebc2e2535643570eae9fec1a7095355d6a71a9
vedika026 <cvedikareddy3@gmail.com>
2025-03-03 20:40:41 +0530

    Update maindraft.py

--- maindraft.py
+++ maindraft.py
@@ -7,4 +7,4 @@ from discord.ext import commands, tasks
 intents=discord.Intents.default()
 intents.message_content=True
 bot=commands.Bot(command_prefix="$", intents=intents)
-TOKEN=os.getenv("")
+TOKEN=("")
